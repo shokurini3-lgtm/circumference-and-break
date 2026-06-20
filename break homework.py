f9c7417097c4120121f73bacd040c14e1ae16941
shokurini3-lgtm <shokurini3@gmail.com>
2026-06-20 14:08:07 +0100

    Add files via upload

---
+++ break homework.py
@@ -0,0 +1,6 @@
+def calculate_change(price,amount_given):
+    return amount_given - price
+
+
+c = calculate_change(130,150)
+print("the change for the customer is £" , c)
---
+++ circumference homework.py
@@ -0,0 +1,7 @@
+import math
+def circumference(radius):
+    return 2 * math.pi * radius
+
+
+c=circumference(10)
+print("circumference is " , c)
